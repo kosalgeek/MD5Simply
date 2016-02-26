abf189f1162f2e7b353aea7fb32b3984826cb8af
kosalgeek <kosalgeek@gmail.com>
2016-02-26 12:22:23 +0700

    first commit

---
+++ MD5Simply/app/src/main/java/com/kosalgeek/android/md5simply/MD5.java
@@ -0,0 +1,39 @@
+package com.kosalgeek.android.md5simply;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Simple MD5 Encryption
+ */
+public final class MD5 {
+
+    /**
+     * Encrypt a text into MD5
+     * @param text
+     * @return MD5 encrypted text
+     */
+    public final static String encrypt(final String text) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = MessageDigest
+                    .getInstance("MD5");
+            digest.update(text.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++) {
+                String h = Integer.toHexString(0xFF & messageDigest[i]);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
